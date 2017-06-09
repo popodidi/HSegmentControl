2e0a0827d1e34bdb6897f64c970c8709d725d896
Hao <changhao@sendbest.cc>
2017-06-09 08:10:55 +0800

    weak datasource no @objc in datasource protocol

--- HSegmentControl/Classes/HSegmentControlDataSource.swift
+++ HSegmentControl/Classes/HSegmentControlDataSource.swift
@@ -8,8 +8,7 @@
 
 import Foundation
 
-@objc
-public protocol HSegmentControlDataSource {
+public protocol HSegmentControlDataSource: class {
     /**
      Retrun the total number of segments
      */
@@ -22,5 +21,11 @@ public protocol HSegmentControlDataSource {
      Return background view of each segment.
      The background view will be set with constraints to fit the size of each segment and will be set to `UIView()` if this method is not implemented.
      */
-    @objc optional func segmentControl(_ segmentControl: HSegmentControl, segmentBackgroundViewOfIndex index: Int) -> UIView
+    func segmentControl(_ segmentControl: HSegmentControl, segmentBackgroundViewOfIndex index: Int) -> UIView
+}
+
+extension HSegmentControlDataSource {
+    func segmentControl(_ segmentControl: HSegmentControl, segmentBackgroundViewOfIndex index: Int) -> UIView {
+        return UIView()
+    }
 }
